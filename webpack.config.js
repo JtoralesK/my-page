309eeb8033ef12b5cd22152f283a4b3eb3713cd5
Javier Torales <82301704+JtoralesK@users.noreply.github.com>
2022-05-15 22:32:03 -0300

    se modifico el comando build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require("path");
 const dev = process.env.NODE_ENV == "development";
+const production = process.env.NODE_ENV == "production";
+
 const live = require("live-server")
 if(dev){
   console.log("estoy en modo local");
+  live.start({
+    root:"./",
+    "file":"public/index.html"
+  })
+}
+if(production){
+  console.log("estoy en modo production");
   live.start({
     root:"./",
     "file":"public/index.html"
